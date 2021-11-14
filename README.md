332a005807d5eeb6e2536e4b59adc068457921de
Ayusummer <1369661643@qq.com>
2021-11-14 11:30:44 +0800

    refactor🎨:  只保留补偿刀成员昵称表的反馈

--- README.md
+++ README.md
@@ -22,16 +22,21 @@ Hoshino 2 模块, 读取 yobot 的 sqlite3 数据库反馈当前会战状态(已
 ---
 ## 逻辑说明
 > 逻辑冗余很多, sql 不熟练🥲
-- 读 `yobotdata.db` 的 `clan_challenge` 表获取最后一个 `pcrdate` 的所有数据写入一个新的表 `table_lastDate` 中
-    > 数据写到新表里主要是不清楚后面会不会有新需求要改所以建了个表把原始数据存下
-- 读 `table_lastDate` 将 `qqid` 和 `boss_health_remain` 打组传给 `data_simple_data`
-- 将 `data_simple_data` 按照 `qqid` 分组生成列表 `data_simple_data_split`, 每组表示一个成员的当日出刀数据
+- 读 `yobotdata.db` 的 `clan_challenge` 表获取最后一个 `pcrdate` 中所有数据的 `qqid, boss_health_ramain, is_continue` 送 `data_lastDate`
+- 将 `data_lastDate` 按照 `qqid` 分组生成列表 `data_simple_data_split`, 每组表示一个成员的当日出刀数据
 - 遍历 `data_simple_data_split`
   - 每组列表根据 `boss_health_remain` 判断是否手上还有补偿刀并计算出该成员已出的完整刀;   
     若手中有补偿刀则将此 qqid 写入 `attacked_half_lst(手中还有补偿刀的 qqid 列表)`
   - 遍历完所有组得出总的整刀数目和补偿刀数目
 - 遍历 `attacked_half_lst` 查 `user` 表得出 `nickname`
-- 反馈出刀情况和补偿刀情况
+- 反馈补偿刀情况
 
-
+---
+## CHANGELOG
+- v1.0.0  
+  - 经群友提醒发现 yobot 今年的 release 中已经有出刀数了, 那索性只留个补偿刀名单就行了(
+    > 才发现个人自用的 yobot 已经落后将近一年的版本了(
+  - 删除多余的完整刀和补偿刀数目的功能, 只保留一个补偿刀名单的功能
+- v0.0.0
+  - 初版, 包含查完整刀, 补偿刀数目以及手中还有补偿刀的成员名单的功能
 
--- clan_status.py
+++ clan_status.py
@@ -28,57 +28,22 @@ async def status(session):
     # select last row's challenge_pcrdate 获取当天的 pcrdate
     curr_date: int = cur.execute(sql_find_last_date).fetchone()[0]
 
-    # 获取当天的所有会战数据
-    sql_get_lastDate_data = "SELECT * FROM clan_challenge \
-                            WHERE challenge_pcrdate = {0} and gid = {1}".format(curr_date, gid)
+    # 获取当天的所有会战数据的 qqid, boss_health_ramain, is_continue 打组
+    sql_get_lastDate_data = "SELECT qqid, boss_health_ramain, is_continue FROM clan_challenge \
+                            WHERE challenge_pcrdate = {0} and gid = {1} \
+                                ORDER BY qqid".format(curr_date, gid)
     data_lastDate = cur.execute(sql_get_lastDate_data).fetchall()
 
-    """ 创建当天的会战信息表
-        这里也可以不建表, 建表主要是为了之后可能存在的功能扩展(大概
-        不建表的话直接转到 line 67 提取 qqid, boss_health_remain 就行了
-    """
-    sql_create_table_lastDate = """CREATE TABLE IF NOT EXISTS table_lastDate 
-                                (  cid INTEGER PRIMARY KEY,
-                                    bid INTEGER,
-                                    gid INTEGER,
-                                    qqid INTEGER,
-                                    challenge_pcrdate INTEGER,
-                                    challenge_pcrtime INTEGER,
-                                    boss_cycle INTEGER,
-                                    boss_num    INTEGER,
-                                    boss_health_remain INTEGER,
-                                    challenge_damage INTEGER,
-                                    is_continue INTEGER,
-                                    message TEXT,
-                                    behalf INTEGER );
-                            """
-    cur.execute(sql_create_table_lastDate)
-
-    # rewrite table_lastDate with data_lastDate 将当天的会战信息写入表
-    sql_rewrite_table_lastDate = "INSERT INTO table_lastDate \
-                                (cid, bid, gid, qqid, challenge_pcrdate, challenge_pcrtime, boss_cycle, boss_num, boss_health_remain, challenge_damage, is_continue, message, behalf) \
-                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-    cur.executemany(sql_rewrite_table_lastDate, data_lastDate)
-
-    # get all date from table_lastDate  获取当天所有的会战信息
-    sql_get_all_lastDate = "SELECT * FROM table_lastDate"
-    data_all_lastDate = cur.execute(sql_get_all_lastDate).fetchall()
-
-    # get [cid, qqid, boss_health_remain, is_continue] from table_lastDate oder by qqid 
-    sql_get_simple_data = "SELECT cid, qqid, boss_health_remain, is_continue FROM table_lastDate \
-                                    ORDER BY qqid"
-    data_simple_data = cur.execute(sql_get_simple_data).fetchall()
-
-    # split data_simple_data by qqid 将会战信息按照 qqid 分组, 每组表示一个成员当天的出刀情况
+    # 将会战信息按照 qqid 分组, 每组表示一个成员当天的出刀情况
     data_simple_data_split = []
-    for i in range(len(data_simple_data)):
+    for i in range(len(data_lastDate)):
         if i == 0:
-            data_simple_data_split.append([data_simple_data[i]])
+            data_simple_data_split.append([data_lastDate[i]])
         else:
-            if data_simple_data[i][1] == data_simple_data[i-1][1]:
-                data_simple_data_split[-1].append(data_simple_data[i])
+            if data_lastDate[i][0] == data_lastDate[i-1][0]:
+                data_simple_data_split[-1].append(data_lastDate[i])
             else:
-                data_simple_data_split.append([data_simple_data[i]])
+                data_simple_data_split.append([data_lastDate[i]])
 
     """ 遍历每组出刀情况获取总的出刀情况
         attack_full: 已出的完整刀的数量
@@ -89,24 +54,23 @@ async def status(session):
     for i in data_simple_data_split:
         full, half = 0, 0
         for j in i:
-            if j[2] == 0:
+            if j[1] == 0:
                 half = 1
             full += 1
-            if j[3] == 1:
+            if j[2] == 1:
                 half = 0
                 full -= 1
         if half == 1:
             full -= 1
-            attacked_half_lst.append(i[0][1])
+            attacked_half_lst.append(i[0][0])
         attacked_full += full
         attacked_half += half
 
-    # find nickname by attacked_half_lst in user table  通过 attacked_half_lst 查 user 表获取成员昵称
+    # 查 user 表获取成员昵称
     sql_find_nickname = "SELECT qqid, nickname FROM user WHERE qqid = ?"
     nickname_lst = []
     for i in attacked_half_lst:
         nickname_lst.append(cur.execute(sql_find_nickname, (i,)).fetchone()[1])
 
-    msg= "当前已出{0}刀完整刀, 还剩{1}刀完整刀和{2}刀补偿刀 \n \
-            手中还有补偿刀的成员为:{3}".format(attacked_full, 90-attacked_half-attacked_full, attacked_half, nickname_lst)
+    msg= "手中还有补偿刀的成员为:{0}".format(nickname_lst)
     await session.send(msg)
